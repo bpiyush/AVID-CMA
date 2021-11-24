cb4c3f231f002dcd5fa9f919d151d45891e03844
Piyush Bagad <piyushnbagad11@gmail.com>
2021-11-24 14:14:54 +0100

    Minor

--- check_conda_env.py
+++ check_conda_env.py
@@ -3,6 +3,9 @@ import torch
 import torchvision
 import numpy as np
 
+print("PyTorch version:\t {}".format(torch.__version__))
+print("Torchvision version:\t {}".format(torchvision.__version__))
+
 x = torch.randn(3, 4)
 
 if torch.cuda.is_available():
