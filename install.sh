c26cd02bfbf4a5ad633838e5a2f9fac63a0de817
Piyush Bagad <piyushnbagad11@gmail.com>
2021-11-24 14:57:55 +0100

    Adds install file to make it easy to setup dependencies

---
+++ install.sh
@@ -0,0 +1,6 @@
+# create conda environment and install dependencies
+conda create --name avid --file conda-spec-list.txt
+conda activate avid
+
+# install additional dependencies
+pip install ipdb librosa pyyaml
