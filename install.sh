b22c490c15e953fc2f3ace608b24d5e6741765a2
Piyush Bagad <piyushnbagad11@gmail.com>
2021-11-25 15:33:40 +0100

    Adds tensorboard dependency to install script

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ conda create --name avid --file conda-spec-list.txt
 conda activate avid
 
 # install additional dependencies
-pip install ipdb librosa pyyaml
+pip install ipdb librosa pyyaml tensorboard
